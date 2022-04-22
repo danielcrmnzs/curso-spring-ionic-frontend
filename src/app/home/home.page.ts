1bc11b3d7b657d6ebd84bdf968cdbd02575ff366
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-04-22 10:35:06 -0300

    Obtendo os dados do formulário de login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CredenciaisDTO } from './../../models/credenciais.dto';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
@@ -8,11 +9,17 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  creds: CredenciaisDTO = {
+    email: '',
+    senha: '',
+  };
+
   constructor(private router: Router, public menu: MenuController) {}
 
   ngOnInit() {}
 
   login() {
+    console.log(this.creds);    
     this.router.navigateByUrl('/categorias');
   }
 
