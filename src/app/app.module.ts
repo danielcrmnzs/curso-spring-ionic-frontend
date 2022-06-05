1be7610dd50d1e75045ce166d0c4da7ac4f0d7db
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-06-05 11:00:54 -0300

    Adicionado configuração de moeda para o brasileiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { httpInterceptorProviders } from './interceptors/index';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
@@ -9,6 +9,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,6 +28,8 @@ import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
     httpInterceptorProviders,
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
     JwtHelperService,
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
   ],
   bootstrap: [AppComponent],
 })
