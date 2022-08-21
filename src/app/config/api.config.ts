f73dc14d166f9de9f1c9a9cf2bc9edd2fb93e214
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-08-20 22:16:19 -0300

    Modificado baseUrl para acessar API externo

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,6 @@
 export const API_CONFIG = {
-  baseUrl: 'http://localhost:8080',
+  //baseUrl: 'http://localhost:8080',
+  baseUrl: 'http://192.168.0.111:8080',
+  //baseUrl: 'https://curso-spring-ionic-dcrm-v2.herokuapp.com',
   bucketBaseUrl: 'https://curso-spring-ionic-dcrm.s3.sa-east-1.amazonaws.com',
 };
